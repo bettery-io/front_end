9bf82b49f4777c1b5bf445ec67d4dd49a5caf907
Voroshilov Max <voroshilovmax90@gmail.com>
2019-11-26 16:29:56 +0700

    added my activites

--- src/app/components/my-activites/my-activites.component.ts
+++ src/app/components/my-activites/my-activites.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../app.state';
 import { Router } from "@angular/router"
+import { PostService } from '../../services/post.service';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'my-activites',
@@ -9,19 +12,80 @@ import { Router } from "@angular/router"
   styleUrls: ['./my-activites.component.sass']
 })
 export class MyActivitesComponent implements OnInit {
+  userWallet: string = undefined;
+  allData: any = [];
+  myActivites: any = [];
+  spinner: boolean = true;
+  hostFilet: boolean = true;
+  parcipiantFilter: boolean = true;
+  validateFilter: boolean = true; 
 
   constructor(
     private store: Store<AppState>,
-    private router: Router
+    private router: Router,
+    private postService: PostService
   ) {
     this.store.select("user").subscribe((x) => {
       if (x.length === 0) {
         this.router.navigate(['~ki339203/home'])
+      } else {
+        this.userWallet = x[0].wallet
       }
     });
   }
 
   ngOnInit() {
+    if (this.userWallet != undefined) {
+      let data = {
+        wallet: this.userWallet
+      }
+      this.postService.post("my_activites", data)
+        .subscribe((x) => {
+          console.log(x);
+          this.myActivites = x;
+          this.allData = x;
+          this.spinner = false;
+        }, (err) => {
+          console.log(err);
+        })
+    }
+  }
+
+  getParticipationTime(data){
+    let date = new Date(data.startTime)
+    return moment(date, "YYYYMMDD").fromNow();
+  }
+
+  getValidationTime(data){
+    let date = new Date(data.endTime)
+    return moment(date, "YYYYMMDD").fromNow();
+  }
+
+  getEndValidation(data){
+    let date = new Date(data.endTime)
+    date.setDate(date.getDate() + 7);
+    return moment(date, "YYYYMMDD").fromNow();
+  }
+
+  filter(){
+    setTimeout(()=>{
+      let data = this.allData
+      if(!this.hostFilet){
+        data = data.filter((x)=> x.from !== "Host");
+      }
+      if(!this.parcipiantFilter){
+        data = data.filter((x)=> x.from !== "Participant");
+      }
+      if(!this.validateFilter){
+        data = data.filter((x)=> x.from !== "Validator");
+      }
+      this.myActivites = data
+    },100)
+  }
+
+  getActiveQuantity(from){
+    let data = this.allData.filter((x)=> x.from === from);
+    return data.length
   }
 
 }

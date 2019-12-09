d5476c086a70a420525ced1eae74c537e2986c3f
Voroshilov Max <voroshilovmax90@gmail.com>
2019-12-09 21:18:13 +0700

    added withdrawal functionality

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -21,9 +21,12 @@ export class NavbarComponent {
   web3: Web3 | undefined = null;
   coinInfo = null;
   depositAmount: number = 0;
+  withdrawalAmount: number = 0;
   depositError: string = undefined;
+  withdrawalError: string = undefined;
   amountSpinner: boolean = true;
   depositSpinner: boolean = false;
+  withdrawalSpinner: boolean = false;
 
 
   constructor(private store: Store<AppState>, private modalService: NgbModal) {
@@ -40,7 +43,7 @@ export class NavbarComponent {
     this.loomEthCoinData = new LoomEthCoin()
     this.loomEthCoinData.load(this.web3)
     setTimeout(() => {
-     this.updateBalance()
+      this.updateBalance()
     }, 2500)
   }
 
@@ -65,26 +68,54 @@ export class NavbarComponent {
   }
 
 
-  changeInput(value){
+  changeInput() {
     this.depositError = undefined;
   }
 
-  async deposit(){
-    if(this.depositAmount > 0){
-      this.depositSpinner = true;
-      var value = this.depositAmount * 1000000000000000000;
-      let response = await this.loomEthCoinData.depositEth(value);
-      if(response === undefined){
-       this.modalService.dismissAll()
-       this.depositSpinner = false;
-      }else{
-        this.depositSpinner = false;
-        this.depositError = response.message
-      }
-    }else{
-     this.depositError = "Must be more than zero"
-    }
+  changeWithdrawal() {
+    this.withdrawalError = undefined;
+  }
 
+  async deposit() {
+    if (this.depositAmount > 0) {
+      if (Number(this.depositAmount) > Number(this.coinInfo.mainNetBalance)) {
+        this.depositError = "You don't have enough money"
+      } else {
+        this.depositSpinner = true;
+        var value = this.web3.utils.toWei(this.depositAmount.toString(), 'ether')
+        let response = await this.loomEthCoinData.depositEth(value);
+        if (response === undefined) {
+          this.modalService.dismissAll()
+          this.depositSpinner = false;
+        } else {
+          this.depositSpinner = false;
+          this.depositError = response.message
+        }
+      }
+    } else {
+      this.depositError = "Must be more than zero"
+    }
+  }
+
+  async withdrawal() {
+    if (this.withdrawalAmount > 0) {
+      if (Number(this.withdrawalAmount) > Number(this.coinInfo.loomBalance)) {
+        this.withdrawalError = "You don't have enough money in Loom network"
+      } else {
+        var value = this.web3.utils.toWei(this.withdrawalAmount.toString(), 'ether')
+        let response = await this.loomEthCoinData.withdrawEth(value)
+        if (response === undefined) {
+          this.modalService.dismissAll()
+          this.withdrawalSpinner = false;
+        } else {
+          this.withdrawalSpinner = false;
+          this.withdrawalError = response.message
+        }
+        console.log(response);
+      }
+    } else {
+      this.withdrawalError = "Must be more than zero"
+    }
   }
 
 

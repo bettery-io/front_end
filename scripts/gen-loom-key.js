e2399d10bfe420a2e2ff7b15cd87c31b694c80b2
Voroshilov Max <voroshilovmax90@gmail.com>
2020-04-13 16:04:29 +0300

    move contract deployer to the backend folder

--- scripts/gen-loom-key.js
+++
@@ -1,6 +0,0 @@
-const { CryptoUtils } = require('loom-js')
-const fs = require('fs')
-
-const privateKey = CryptoUtils.generatePrivateKey()
-const privateKeyString = CryptoUtils.Uint8ArrayToB64(privateKey)
-fs.writeFileSync('./private_key', privateKeyString)
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,8 @@ import { ErcCoinSaleComponent } from './components/erc-coin-sale/erc-coin-sale.c
 
 
 const routes: Routes = [
+  // ~ki339203 only for test hosting
+  // remove before deploy to production
   { path: "~ki339203/home", component: HomeComponent },
   { path: "~ki339203", redirectTo: "~ki339203/home", pathMatch: "full" },
   { path: "~ki339203/create-quize", component: CreateQuizeComponent },

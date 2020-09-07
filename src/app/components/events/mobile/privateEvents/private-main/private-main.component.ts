cf252ee0c6c2be05c76e720f2c39af47e153d955
andrii <asohrin9996@gmail.com>
2020-09-07 18:24:07 +0300

    First commit

--- src/app/components/events/mobile/privateEvents/private-main/private-main.component.ts
+++ src/app/components/events/mobile/privateEvents/private-main/private-main.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostService} from "../../../../../services/post.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-private-main',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./private-main.component.sass']
 })
 export class PrivateMainComponent implements OnInit {
+  data: any;
 
-  constructor() { }
+  answerForm: FormGroup;
+
+  condition: boolean = false;
+  counts: any = 1;
+  expert: boolean = false;
+
+  fakeAnswer: string = '';
+  fakeEndDate: number = 1;
+
+  constructor(private postService: PostService, private formBuilder: FormBuilder) {
+    this.postService.post('privateEvents/get_by_id', {id: 457396837154828}).subscribe(value => this.data = value);
+
+    this.answerForm = formBuilder.group({
+      answer: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  changePage() {
+    // this.data.endTime <= 0 ? this.expert = true : this.expert = false;
+    this.fakeEndDate <= 0 ? this.expert = true : this.expert = false;
+
+    this.condition = true;
+  }
+
+  sendAnswer(answerForm: any) {
+    console.log(answerForm.value);
+    this.counts = 3;
+    this.fakeAnswer = answerForm.value.answer;
+  }
+
+  prevPage() {
+    this.counts = 1;
+  }
+
+  nextPage() {
+    this.counts = 2;
+  }
 }

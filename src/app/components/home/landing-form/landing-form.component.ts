ee24de5c59221816e0f00684a3bce0426d5b2a70
andrii <asohrin9996@gmail.com>
2020-09-23 17:52:50 +0300

    commit

--- src/app/components/home/landing-form/landing-form.component.ts
+++ src/app/components/home/landing-form/landing-form.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
+import emailjs, {EmailJSResponseStatus} from 'emailjs-com';
+
 @Component({
   selector: 'app-landing-form',
   templateUrl: './landing-form.component.html',
@@ -8,26 +10,48 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class LandingFormComponent implements OnInit {
   form: FormGroup;
-  formValid: boolean;
+  submitted = false;
+  sendMessage: boolean;
+
 
   constructor(private formBuilder: FormBuilder) {
     this.form = formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      email: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
       phoneNumber: ['', Validators.required],
       firm: ['', Validators.required],
     });
   }
 
+  get f() {
+    return this.form.controls;
+  }
+
   ngOnInit(): void {
   }
 
-  sendForm(form: FormGroup) {
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('service_0rs3zi2', 'template_bbonnwn', e.target as HTMLFormElement, 'user_zbn8o6eDZLmq7pZjLA84D')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        if (result.text === 'OK') {
+          this.sendMessage = true;
+        }
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+
+  sendForm(form: FormGroup, $event) {
+    this.submitted = true;
     if (form.status === 'INVALID') {
-      this.formValid = true;
       return;
     }
+    this.sendEmail($event);
     console.log(form);
   }
+
+
 }

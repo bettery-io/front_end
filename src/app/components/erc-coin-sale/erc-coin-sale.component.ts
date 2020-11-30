a47bcd9e66202582d928a2d27df0ba0e4ab04842
Voroshilov Max <voroshilovmax90@gmail.com>
2020-11-30 23:17:10 +0200

    clean code

--- src/app/components/erc-coin-sale/erc-coin-sale.component.ts
+++ src/app/components/erc-coin-sale/erc-coin-sale.component.ts
@@ -1,114 +1,94 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import Web3 from 'web3';
-import Contract from '../../contract/contract.js'
-import maticInit from '../../contract/maticInit'
-
+import { GetService } from '../../services/get.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'erc-coin-sale',
   templateUrl: './erc-coin-sale.component.html',
   styleUrls: ['./erc-coin-sale.component.sass']
 })
-export class ErcCoinSaleComponent implements OnInit {
+export class ErcCoinSaleComponent implements OnInit, OnDestroy {
   private tokenPricePrivate: number = 0;
-
-  tokenPrice: number = 0.001;
-  progressToken: number = 0
-  tokenSold: number = 0;
-  metamaskError: string = undefined
-  token: any = null;
-  tokensAvailable: number = 750000000;
   tokenSale: any = null;
-  numberOfTokens = 1;
-  numberError: boolean = false
-  spinner: boolean = true;
-  userWallet = null;
-  buyTokensMessage: boolean = false;
-  transferButton: boolean = false;
-  verifier: string = undefined;
 
-  constructor() { }
+  // new variable 
+  web3: Web3 | undefined = null;
+  errorMessage: string = undefined
+  getServiceSub: Subscription
+  numberOfTokens = undefined;
 
-  async ngOnInit() { }
 
-  async initToken() {
-    this.userWallet = "TODO";
-    this.verifier = "metamask";
-    this.sellContract(this.userWallet);
-    this.tokenContract();
-    let matic = new maticInit(this.verifier);
-    let account = await matic.getUserAccount();
-    this.transferButton = account === "0xF02B362cBEFC2d5bD5f7C3dBdbD0DE84508525D5";
-  }
+  constructor(public getService: GetService) { }
 
-  async sellContract(wallet) {
-    let contract = new Contract();
-    this.tokenSale = await contract.tokenSaleMainETH(this.verifier)
-    let tokenSold = await this.tokenSale.methods.tokensSold().call();
-    let web3 = new Web3();
-    this.tokenSold = Number(web3.utils.fromWei(tokenSold, 'ether'));
-
-    this.progressToken = (Math.ceil(this.tokenSold) / this.tokensAvailable) * 100;
-
-    let price = await this.tokenSale.methods.tokenPrice().call();
-    this.tokenPricePrivate = Number(price);
-    this.tokenPrice = Number(web3.utils.fromWei(price, "ether"));
-
-    // Detect Sell event
-    this.tokenSale.events.Sell(async (err, event) => {
-      if (err) {
-        console.log(err)
-      } else {
-        this.sellContract(wallet);
-        this.tokenContract();
-      }
+  async ngOnInit() {
+    this.getServiceSub = this.getService.get("tokensale/info").subscribe((x) => {
+      console.log(x);
+    }, (err) => {
+      console.log(err)
     })
   }
 
-  async tokenContract() {
-    let contract = new Contract();
-    this.token = await contract.tokenContractMainETH(this.verifier)
-    this.spinner = false;
-    this.buyTokensMessage = false;
-  }
-
-  async buyToken() {
-    if (this.numberOfTokens <= 0) {
-      this.numberError = true
+  async connectToMetamask() {
+    // Check if MetaMask is installed
+    if (!(window as any).ethereum) {
+      this.errorMessage = "For buying coins you must have Metamask installed.";
     } else {
-      this.numberError = false
-      try {
-        this.buyTokensMessage = true;
-        this.spinner = true;
-        let web3 = new Web3()
-        let amount = web3.utils.toWei(String(this.numberOfTokens), 'ether');
-        await this.tokenSale.methods.buyTokens(amount).send({
-          from: this.userWallet,
-          value: this.numberOfTokens * this.tokenPricePrivate,
-          gas: 500000
-        })
 
-      } catch (err) {
-        this.spinner = false;
-        this.buyTokensMessage = false;
-        if (err.code !== 4001) {
-          this.metamaskError = 'Something went wrong check console';
+      if (!this.web3) {
+        try {
+          await (window as any).ethereum.enable();
+          window.web3 = new Web3(window.web3.currentProvider);
+          this.web3 = new Web3(window.web3.currentProvider);
+        } catch (error) {
+          this.errorMessage = "You need to allow MetaMask.";
         }
-        console.log(err);
+      }
+
+      const coinbase = await this.web3.eth.getCoinbase();
+      if (!coinbase) {
+        this.errorMessage = "Please activate MetaMask first.";
+        return;
+      } else {
+        let checkNetwork = await window.web3._provider.networkVersion
+        if (checkNetwork != '5') {
+          this.errorMessage = "Plaese switch your network in MetaMask to the Main network."
+        } else {
+          this.buyToken(coinbase)
+        }
       }
     }
   }
 
-  // transfer tokens. Avaliable only for owners
-  async transferToken() {
-    let web3 = new Web3()
-    let amount = web3.utils.toWei('750000000', 'ether');
-    let contract = new Contract();
-    let address = contract.tokenContractAddressMainETH()
-    let test = await this.token.methods.transfer(address, amount).send({
-      from: this.userWallet,
-    })
-    console.log(test);
+
+  async buyToken(wallet) {
+    console.log(this.numberOfTokens);
+    if (this.numberOfTokens <= 0) {
+      console.log("work")
+      this.errorMessage = "The value must be bigger than 0"
+    } else {
+      this.errorMessage = undefined;
+      // try {
+      //   let web3 = new Web3()
+      //   let amount = web3.utils.toWei(String(this.numberOfTokens), 'ether');
+      //   await this.tokenSale.methods.buyTokens(amount).send({
+      //     from: wallet,
+      //     value: this.numberOfTokens * this.tokenPricePrivate,
+      //     gas: 500000
+      //   })
+
+      // } catch (err) {
+      //   if (err.code !== 4001) {
+      //     this.errorMessage = 'Something went wrong check console';
+      //   }
+      //   console.log(err);
+      // }
+    }
   }
 
+  ngOnDestroy() {
+    if (this.getServiceSub) {
+      this.getServiceSub.unsubscribe();
+    }
+  }
 }

8cbbdc3a6c3cd38317863f2f11f04c06e076490c
andrii <asohrin9996@gmail.com>
2021-01-11 17:12:53 +0200

    3th fixed active style

--- src/app/components/navigation/navigation.module.ts
+++ src/app/components/navigation/navigation.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NavbarComponent} from './navbar/navbar.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
+import {RouterModule} from '@angular/router';
 
 
 
@@ -14,8 +15,9 @@ import {SidebarComponent} from './sidebar/sidebar.component';
     NavbarComponent,
     SidebarComponent
   ],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        RouterModule
+    ]
 })
 export class NavigationModule { }
--- src/app/components/navigation/sidebar/sidebar.component.ts
+++ src/app/components/navigation/sidebar/sidebar.component.ts
@@ -1,22 +1,59 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, DoCheck, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.sass']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, DoCheck {
   display: boolean;
+  scrollTop: number;
+  currentPath
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
     this.detectPath();
   }
 
-  detectPath(): void {
-    const path = window.location.pathname;
-    this.display = !(path === '/' || path === 'tokensale' || path === '' || path.includes('create-event') || path.includes('private_event')
-      || path.includes('public_event'));
+  ngDoCheck() {
+    this.detectPath();
+    console.log(this.currentPath);
+  }
+
+  detectPath() {
+    this.currentPath = window.location.pathname;
+    if (this.currentPath === '/' || this.currentPath === '/tokensale' || this.currentPath.includes('create-event') || this.currentPath.includes('public_event') ||
+      this.currentPath.includes('private_event')) {
+      this.display = false;
+    } else {
+      this.display = true;
+    }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  listenScroll() {
+    if (this.display) {
+      console.log(document.documentElement.scrollTop);
+      this.scrollTop = document.documentElement.scrollTop;
+    }
+  }
+
+  sidebarTopPosition() {
+    if (document.documentElement.scrollTop < 87) {
+      return {'top': (87 - this.scrollTop) + 'px'};
+    } else {
+      return {'top': 0};
+    }
+  }
+
+
+  detectPathForActive(str: string) {
+    if (this.currentPath === '/' + str) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }

f2c06151e4b6ec0494a08167524d35177a1f74d9
andrii <asohrin9996@gmail.com>
2021-02-02 11:27:45 +0200

    2th coming-soon.component finish

--- src/app/components/share/coming-soon/coming-soon.component.ts
+++ src/app/components/share/coming-soon/coming-soon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-coming-soon',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComingSoonComponent implements OnInit {
 
-  constructor() { }
+  currentPath: string;
+
+  constructor() {
+    this.currentPath = window.location.pathname;
+  }
 
   ngOnInit(): void {
   }
-
 }

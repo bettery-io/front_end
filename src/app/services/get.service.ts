648897bcbbea11163d4828f0e23c62a765102ecd
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-02-22 19:12:40 +0200

    added public API

--- src/app/services/get.service.ts
+++ src/app/services/get.service.ts
@@ -8,7 +8,7 @@ export class GetService {
 
   constructor(private http: HttpClient) { }
 
-  url = environment.production ? 'https://13.212.36.199' : 'https://13.229.200.135';
+  url = environment.production ? 'https://api.bettery.io' : 'https://13.229.200.135';
   //url = "http://localhost:80"
 
   get(path: string) {
